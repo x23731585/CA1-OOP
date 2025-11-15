4c0b3bc717d81552b222426b29ae9a3bed4582ac
Alexb <x23731585@student.ncirl.ie>
2025-11-15 01:53:43 +0000

    CA1 PROJECT SRTUCTURE

---
+++ src/APP/MAINAPP.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package APP;
+
+/**
+ *
+ * Data Class
+Purpose: Define what information is stored about each run
+* 
+* Compute/Logic Class
+Purpose: Add runs, calculate totals or averages, manage data.
+* 
+* UI Class (JFrame)
+Purpose: Show input fields, buttons, and display output to the user.
+ */
+public class MAINAPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
