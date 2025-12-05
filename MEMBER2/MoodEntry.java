1835926f576bbbb7775acff49c114696d55b850d
Desmond1up <153213527+Desmond1up@users.noreply.github.com>
2025-12-05 20:23:29 +0000

    Add files via upload

---
+++ MEMBER2/MoodEntry.java
@@ -0,0 +1,35 @@
+//  Desmond 
+package mentalhealthhub;
+
+import java.time.LocalDate;
+
+// This stores one mood entry for a given day
+public class MoodEntry {
+
+    private LocalDate date;
+    private String mood;
+    private String notes;
+
+    public MoodEntry(LocalDate date, String mood, String notes) {
+        this.date = date;
+        this.mood = mood;
+        this.notes = notes;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    @Override
+    public String toString() {
+        return date + " - " + mood + " (" + notes + ")";
+    }
+}
